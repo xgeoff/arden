f033904f58a8f7ad47f09ac5746f0bd7f2525a89
Jefe <xgeoff@gmail.com>
2025-09-02 21:00:26 -0700

    Parse Groovy front matter titles

--- site/helpers/pages.groovy
+++ site/helpers/pages.groovy
@@ -1,5 +1,6 @@
 import com.github.jknack.handlebars.Helper
 import com.github.jknack.handlebars.Options
+import groovy.util.ConfigSlurper
 
 return { Object context, Options options ->
     def pagesDir = options.context.get('pagesDir') as File
@@ -25,11 +26,39 @@ return { Object context, Options options ->
                 def relativePath = root.toPath().relativize(file.toPath()).toString().replace(File.separator, "/")
                 def name = file.name.replaceFirst(/(?i)\.(md|html?)$/, "")
 
-                items << [
+                // Look for a Groovy front matter block and extract the title when present
+                def title
+                if (file.name.toLowerCase().endsWith(".md")) {
+                    def lines = file.readLines()
+                    if (lines && lines[0] ==~ /^---\s*$/) {
+                        def fmLines = []
+                        for (int i = 1; i < lines.size(); i++) {
+                            def line = lines[i]
+                            if (line ==~ /^---\s*$/) {
+                                break
+                            }
+                            fmLines << line
+                        }
+                        if (fmLines) {
+                            try {
+                                def config = new ConfigSlurper().parse(fmLines.join("\n"))
+                                title = config.title?.toString()
+                            } catch (ignored) {
+                                // ignore malformed front matter
+                            }
+                        }
+                    }
+                }
+
+                def item = [
                         type: 'file',
                         name: name,
                         path: relativePath
                 ]
+                if (title) {
+                    item.title = title
+                }
+                items << item
             }
         }
 
